53b2d257f15ae5afc73e14d9af54ccc57b0e0e96
joao-victor-dias <joaovictor9.7@hotmail.com>
2022-03-29 16:38:27 -0300

    Commit Inicial

---
+++ Questão1/Classes/CartaoBandeiraMaster.cs
@@ -0,0 +1,31 @@
+﻿using Questão1.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Questão1.Classes
+{
+    public class CartaoBandeiraMaster : IPagamentoCartao
+    {
+        private string TipoDePagamento { get; set; }
+        private string BandeiraCartao { get; set; }
+
+        public CartaoBandeiraMaster(string TipoDePagamento)
+        {
+            this.TipoDePagamento = TipoDePagamento;
+            this.BandeiraCartao = "Master";
+        }
+        
+        public void pagamentoCredito()
+        {
+            Console.WriteLine($"Tipo de Pagamento: Crédito | Pago com Master ");
+        }
+
+        public void pagamentoDebito()
+        {
+            Console.WriteLine($"Tipo de Pagamento: Debito | Pago com Master ");
+        }
+    }
+}
---
+++ Questão1/Classes/CartaoBandeiraVisa.cs
@@ -0,0 +1,31 @@
+﻿using Questão1.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Questão1.Classes
+{
+    public class CartaoBandeiraVisa : IPagamentoCartao
+    {
+        private string TipoDePagamento { get; set; }
+        private string BandeiraCartao { get; set; }
+
+        public CartaoBandeiraVisa(string TipoDePagamento)
+        {
+            this.TipoDePagamento = TipoDePagamento;
+            this.BandeiraCartao = "Visa";
+        }
+
+        public void pagamentoCredito()
+        {
+            Console.WriteLine($"Tipo de Pagamento: Crédito | Pago com Visa ");
+        }
+
+        public void pagamentoDebito()
+        {
+            Console.WriteLine($"Tipo de Pagamento: Debito | Pago com Visa ");
+        }
+    }
+}

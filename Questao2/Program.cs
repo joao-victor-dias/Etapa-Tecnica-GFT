86e55c03081f7a5b8af070ec3e7fc5100ed48902
joao-victor-dias <joaovictor9.7@hotmail.com>
2022-03-29 18:07:42 -0300

    commit 3

--- Questao2/Program.cs
+++ Questao2/Program.cs
@@ -2,11 +2,26 @@
 
 namespace Questao2
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            int[] vetor = new vetor[]; 
+            Console.WriteLine("Insira o tamanho do vetor");
+            int tamanhoVetor = int.Parse(Console.ReadLine());
+            int[] vetor = new int[tamanhoVetor];
+
+            for (int i = 0; i < vetor.Length; i++)
+            {
+                Console.WriteLine("Insira o valor");
+                int valor = int.Parse(Console.ReadLine());
+                foreach (int v in vetor)
+                    if (v != valor || v == null)
+                        vetor[i] = valor;
+                Console.WriteLine("Valor Invalido");
+            }
+
+            foreach (int i in vetor)
+                Console.Write($"Vetor:{i} ");
         }
     }
 }
